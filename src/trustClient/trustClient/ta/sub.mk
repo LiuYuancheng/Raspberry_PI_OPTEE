0cfec4b3aa88196993cb856b2a493e01bb6019f5
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2019-05-31 10:40:16 +0800

    New trust client use AES256 encrypt.

---
+++ src/trustClient/trustClient/ta/sub.mk
@@ -0,0 +1,6 @@
+global-incdirs-y += include
+#global-incdirs-y += ../host/include
+srcs-y += aes_ta.c
+
+# To remove a certain compiler flag, add a line like this
+#cflags-template_ta.c-y += -Wno-strict-prototypes
