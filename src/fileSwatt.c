ae573f8039a906fd88a6695ecd14cae297b39420
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2019-06-12 14:29:36 +0800

    use Linear congruential generator

--- src/fileSwatt.c
+++ src/fileSwatt.c
@@ -2,6 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* always assuming int is at least 32 bits */
+int bsd_rand();
+int rseed = 0;
+ 
+void bsd_srand(int x)
+{
+	rseed = x;
+}
+
+#define BSD_RAND_MAX ((1U << 31) - 1)
+ 
+int bsd_rand()
+{
+	return rseed = (rseed * 1103515245 + 12345) & BSD_RAND_MAX;
+}
+
+
 char *readFileBytes(const char *name)
 {
     FILE *fl = fopen(name, "r");
@@ -41,6 +58,12 @@ int getSWATT(void)
         state[i] = state[j];
         state[j] = tmp;
     }
+    
+    //for (int i = 0; i < m; i++)
+    //{
+    //   printf("IP:<%d>\n", state[i]);
+    //}
+
 
     int s = 1;
     int k = 16;
@@ -50,8 +73,7 @@ int getSWATT(void)
     for (int t = 0; t < keylen; t++)
     {
         test[t] = challengeInt[t] ^ final;
-
-        printf(":<%d>\n", test[t]);
+        //printf(":<%d>\n", test[t]);
     }
 
     for (int t = 0; t < keylen; t++)
@@ -74,13 +96,22 @@ int getSWATT(void)
         swatt_seed = cr_response ^ init_seed;
         int Address = (state[i] << 8)+prev_cs;
         //printf("AD:<%d>\n", Address);
-        srand(Address);
-        int M = 1;
-        int N = 128000;
-        int num = M + rand() / (RAND_MAX / (N - M + 1) + 1);
-        //printf("RA:<%d>\n", num);
-        char strTemp = ret[num];
-        current_cs = current_cs +((int)strTemp ^ pprev_cs+state[i-1]);
+        bsd_srand(Address);
+        Address = bsd_rand()%128000+1;
+        //printf("AD:<%d>\n", Address);
+        char strTemp = ret[Address];
+        
+        //printf("R2:<%c>\n", strTemp);
+        //printf("R3:<%d>\n", current_cs);
+        //printf("R4:<%d>\n", pprev_cs);
+        
+        int num = i-1;
+        if(num<0){
+            num = m-1;
+        }
+        
+        current_cs = current_cs +((int)strTemp ^ pprev_cs + state[num]);
+        //printf("R5:<%d>\n", current_cs);
         init_seed = current_cs+swatt_seed;
         current_cs = current_cs >> 1; 
         pprev_cs = prev_cs;
