fb9db1e121d089d870def381f7586460d25a1a22
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2019-06-14 17:31:54 +0800

    Added the swatt value verification part.

--- src/taServer.py
+++ src/taServer.py
@@ -13,12 +13,24 @@
 import socket
 import threading
 import time
-from Crypto import Random
+import IOT_Att as SWATT
+
+# if got error install Crypto please usethis: 
+#   >> pip install pycryptodome
+#   from Crypto.Cipher import AES  #Works
+# or 
+#   >> pip install pycryptodomex
+#   from Cryptodome.Cipher import AES 
+# For python3 the package name is now pycryptodome or pycryptodomex
+# If you need compatibility with your project with Python2 use pycryptodome
+# or else use pycryptodomex which is a library independent of the old PyCrypto.
+# https://stackoverflow.com/questions/51824628/modulenotfounderror-no-module-named-crypto-error
 from Crypto.Cipher import AES
 
 
 bind_ip = '0.0.0.0'
 bind_port = 5007
+CHA_LEN = 7     # default challenge we are going to use. 
 
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
@@ -48,6 +60,10 @@ class taServer(object):
         self.server.bind((bind_ip, bind_port))
         self.server.listen(5)  # max backlog of connections
         print ('Listening on {}:{}'.format(bind_ip, bind_port))
+        # Calculate the SWATT value
+        self.challengeStr = ""
+        self.swattHd = SWATT.swattCal()
+        self.swattHd.setPuff(1549465112)
 
         # fixed AES key which is same as the trust APP's TA
         self.key = bytes([0xa5]*32)
@@ -60,17 +76,36 @@ class taServer(object):
         print (self.decrypted)
         print ("Init finished")
 
+    def setSwattChalStr(self, challengeStr):
+
+        if len(challengeStr) < CHA_LEN: 
+            print("The challenge string is not long enough.")
+            return
+        self.challengeStr = challengeStr[:CHA_LEN]
+        self.challengeB = bytes([ord(n) for n in self.challengeStr] + [ord('Z')]*25)
+        self.encrypted = self.cipher.encrypt(self.challengeB)
+        print(self.challengeB)
+            
 #-----------------------------------------------------------------------------
     def handle_client_connection(self, client_socket):
         request = client_socket.recv(32)
         print ('Received {}'.format(request))
         print ('Send the encrypted challenge bytes')
         client_socket.send(self.encrypted)
-        
+        # Calcualte the SWATT value for verification.
+        result = self.swattHd.getSWATT(self.challengeStr, 300, "firmwareSample")
         print ('Received the SWATT bytes and decode:')
         request = client_socket.recv(32)
         data = self.cipher.decrypt(request)
+        print(result)
+        print("-------------")
+        data = str(data).split('x0')[0]
         print(data)
+        # The back data example is b'4098\x00ZZZZZZZZZZ'
+        if int(data[2:-1]) == result: 
+            print("The file Swatt value has been verified.")
+        else: 
+            print("The value are differet:"+str((data[2:-1]), result))
 
 #-----------------------------------------------------------------------------
     def startServer(self):
@@ -78,13 +113,15 @@ class taServer(object):
             print('Wait for connection')
             client_sock, address = self.server.accept()
             print ('Accepted connection from {}:{}'.format(address[0], address[1]))
-            target = self.handle_client_connection(client_sock)
+            self.handle_client_connection(client_sock)
             client_sock.close()
 
 #-----------------------------------------------------------------------------
 def startServ():
     server = taServer()
+
     print("Server inited.")
+    server.setSwattChalStr("abdcedfs")
     server.startServer()
 
 if __name__ == '__main__':
